04df7c1cb43b3c7da6fa8d233edfb26615260683
Xiang Zhao <xiang.umass@gmail.com>
2014-02-01 17:13:25 -0500

    added a method for getting procefureinstancenodes

--- laser/littlejil/smartchecklist/gui/AgendaItemWindow.java
+++ laser/littlejil/smartchecklist/gui/AgendaItemWindow.java
@@ -1,5 +1,8 @@
 package laser.littlejil.smartchecklist.gui;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -16,22 +19,38 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.layout.GridData;
 
-import java.util.ArrayList;
+import laser.ddg.ProcedureInstanceNode;
+import laser.ddg.ProvenanceData;
+import laser.juliette.ams.AMSException;
+import laser.juliette.ddgbuilder.DDGBuilder;
+import laser.juliette.ddgbuilder.StepReference;
+import laser.juliette.runner.ams.AgendaItem;
+import laser.lj.Step;
 
-import laser.juliette.ams.AgendaItem;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 
 public class AgendaItemWindow {
 
+	AgendaItem agendaItem;
 	protected Shell shell;
 	private Text text;
 	private Text text_1;
 	private Text text_2;
 	private Text text_3;
 
+	public AgendaItemWindow(AgendaItem agendaItem) {
+		super();
+		this.agendaItem = agendaItem;
+	}
+
+	public AgendaItemWindow() {
+		// TODO Auto-generated constructor stub
+	}
+
 	/**
 	 * Launch the application.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -62,58 +81,89 @@ public class AgendaItemWindow {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		//java.util.List<AgendaItem> agendaItems = new ArrayList<AgendaItem>();
+		// java.util.List<AgendaItem> agendaItems = new ArrayList<AgendaItem>();
 		String[][] agendaItems = new String[3][4];
 		agendaItems[0][0] = "optional";
 		agendaItems[0][1] = "title of agenda item";
 		agendaItems[0][2] = "4/5/16";
 		agendaItems[0][3] = "chris";
-		
+
 		agendaItems[1][0] = "optional";
 		agendaItems[1][1] = "title of agenda item";
 		agendaItems[1][2] = "4/5/16";
 		agendaItems[1][3] = "chris";
-		
+
 		agendaItems[2][0] = "optional";
 		agendaItems[2][1] = "title of agenda item";
 		agendaItems[2][2] = "4/5/16";
 		agendaItems[2][3] = "chris";
-		
+
 		shell = new Shell();
-		shell.setSize(450,300);
+		shell.setSize(450, 300);
 		shell.setLayout(new FillLayout());
-		
-		ScrolledComposite scrolledComposite = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+
+		ScrolledComposite scrolledComposite = new ScrolledComposite(shell,
+				SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		scrolledComposite.setExpandHorizontal(true);
 		scrolledComposite.setExpandVertical(true);
-		
+
 		Composite composite_1 = new Composite(scrolledComposite, SWT.NONE);
 		composite_1.setLayout(new RowLayout(SWT.VERTICAL));
-		
-		for (String[] agendaItem : agendaItems){
+
+		for (String[] agendaItem : agendaItems) {
 			createAgendaItemRow(agendaItem, composite_1);
 		}
-	
+
 		scrolledComposite.setContent(composite_1);
-		scrolledComposite.setMinSize(composite_1.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		scrolledComposite.setMinSize(composite_1.computeSize(SWT.DEFAULT,
+				SWT.DEFAULT));
 	}
-	
-	private Control createAgendaItemRow(String[] agendaItem, Composite parent){
+
+	private Control createAgendaItemRow(String[] agendaItem, Composite parent) {
 		Composite composite = new Composite(parent, SWT.NONE);
 		composite.setLayout(new RowLayout(SWT.HORIZONTAL));
-		
+
 		Button button = new Button(composite, SWT.NONE);
 		button.setText(agendaItem[0]);
-		
+
 		Button button_1 = new Button(composite, SWT.NONE);
 		button_1.setText(agendaItem[1]);
-		
+
 		Button button_2 = new Button(composite, SWT.NONE);
 		button_2.setText(agendaItem[2]);
-		
-		Button button_3 = new Button(composite,SWT.NONE);
+
+		Button button_3 = new Button(composite, SWT.NONE);
 		button_3.setText(agendaItem[3]);
-		
+
 		return composite;
+	}
+
+	// get a list of ProcedureInstanceNodes
+	public ArrayList<ProcedureInstanceNode> getProcedureInstanceNode() {
+		ArrayList<ProcedureInstanceNode> res = new ArrayList<ProcedureInstanceNode>();
+		ProvenanceData pd = ((DDGBuilder) agendaItem.getDdgbuilder())
+				.getProvData();
+		Iterator<ProcedureInstanceNode> pinIter = pd.pinIter();
+		while (pinIter.hasNext()) {
+			ProcedureInstanceNode pin = pinIter.next();
+			if (!pin.getType().equals("Leaf"))
+				continue;
+			if (pin.getProcedureDefinition() instanceof StepReference) {
+				StepReference sr = (StepReference) pin.getProcedureDefinition();
+				try {
+					if (sr.getStepDef().getStepDeclaration()
+							.equals(agendaItem.getStep().getStepDeclaration())) {
+						res.add(pin);
+					}
+				} catch (UnsupportedOperationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (AMSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return res;
 	}
 }
